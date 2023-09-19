1f5fbb306a3720d676ba8f74fc351c432762c8dd
saidazimov720 <saidazimovnurbek01@gmail.com>
2023-09-19 17:26:27 +0500

    add the files

---
+++ script.js
@@ -0,0 +1,31 @@
+const productsURL = "https://fakestoreapi.com/products";
+const sartsURL = "https://fakestoreapi.com/carts";
+const usersURL = "https://fakestoreapi.com/users";
+const result = document.getElementById("res");
+
+function getProducts() {
+    fetch(productsURL)
+    .then((res) => res.json())
+    .then((data) => showProducts(data));
+}
+
+function showProducts(arr) {
+    console.log(arr);
+    result.innerHTML = "";
+    result.innerHTML = arr.map((item) => {
+        const {id, title, price, category, description, image} = item;
+        return `
+        <div class="card">
+        <img src="${image}" alt="${title}">
+        <h2>${title}</h2>
+        <span>${price}</span>
+        <span>${category}</span>
+        <p>${description}</p>
+        <button onlick="updateProduct(${id})">Update</button>
+        <button onlick="updateProduct(${id})">Delete</button>
+        </div>
+        `;
+    });
+}
+
+getProducts();
