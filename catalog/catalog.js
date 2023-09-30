b082f233770e5481a17f0617fb990b8388a24704
saidazimov720 <saidazimovnurbek01@gmail.com>
2023-09-30 15:29:05 +0500

    add the catalog and the other corrections

---
+++ catalog/catalog.js
@@ -0,0 +1,30 @@
+const productsURL = "https://fakestoreapi.com/products?limit=8";
+const sartsURL = "https://fakestoreapi.com/carts";
+const usersURL = "https://fakestoreapi.com/users";
+const result = document.getElementById("res");
+const loding = document.getElementById("loding");
+
+
+function getProducts() {
+    fetch(productsURL)
+    .then((res) => res.json())
+    .then((data) => showProducts(data));
+}
+
+function showProducts(arr) {
+    console.log(arr);
+    result.innerHTML = "";
+    result.innerHTML = arr.map((item) => {
+        const {id, title, price,  image} = item;
+        return `
+        <div class="card">
+        <img src="${image}" alt="${title}">
+        <h2>${title}</h2>
+        <span>${price}$</span><br>
+        <button onclick="infohandler(${id})">More Info</button>
+        </div>
+        `
+    }).join(" ");
+}
+
+getProducts();
