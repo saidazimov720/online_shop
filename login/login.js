5f16015651f0269e3d7c154a67be34bf30b68d45
saidazimov720 <saidazimovnurbek01@gmail.com>
2023-10-05 17:01:57 +0500

    fix the bugs

--- login/login.js
+++ login/login.js
@@ -1,27 +1,38 @@
-
- var users = [];
-
-
- function handleSubmit() {
-     
-     var username = document.getElementById("user").value;
-     var password = document.getElementById("password").value;
-     var email = document.getElementById("email").value;
-
-     
-     var user = {
-         username: username,
-         password: password,
-         email: email
-     };
-
-     
-     users.push(user);
-
-     document.getElementById("username").value = "";
-     document.getElementById("password").value = "";
-     document.getElementById("email").value = "";
-
-     // Saqlansa chiqar shuni   
-     alert("User registered successfully!");
- }
+function addusers() {
+    const username = document.getElementById("user").value;
+    const userEmail = document.getElementById("email").value;
+    const userPassword = document.getElementById("password").value;
+  
+    fetch("https://fakestoreapi.com/users", {
+      method: "POST",
+      body: JSON.stringify({
+        email: userEmail,
+        username: username,
+        password: userPassword,
+        name: {
+          firstname: username,
+        },
+        address: {
+          city: "kilcoole",
+          street: "7835 new road",
+          number: 3,
+          zipcode: "12926-3874",
+          geolocation: {
+            lat: "-37.3159",
+            long: "81.1496",
+          },
+        },
+        phone: "1-570-236-7033",
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        localStorage.setItem("user", JSON.stringify(json));
+        console.log("Foydalanuvchi saqlandi:", json);
+      })
+      .catch((error) => {
+        console.error("Xatolik:", error);
+      });
+  }
+  
+  addusers();
