ed69bc8224369ad5194494711690285b079d0f31
saidazimov720 <saidazimovnurbek01@gmail.com>
2023-10-08 16:24:48 +0500

    corrections

--- basket/catalog.js
+++ basket/catalog.js
@@ -1,25 +1,3 @@
-// function addTask() {
-//     const taskInput = document.getElementById('newTask');
-//     const taskText = taskInput.value.trim();
-
-//     if (taskText !== '') {
-//         const tasksList = document.getElementById('tasks');
-//         const listItem = document.createElement('li');
-//         listItem.innerHTML = `
-//             ${taskText}
-//             <button onclick="deleteTask(this)">Delete</button>
-//         `;
-//         tasksList.appendChild(listItem);
-//         taskInput.value = '';
-//     }
-// }
-
-// function deleteTask(button) {
-//     const listItem = button.parentElement;
-//     const tasksList = document.getElementById('tasks');
-//     tasksList.removeChild(listItem);
-// }
-
 const spinner = document.getElementById("spinner");
 const Basket_res = document.getElementById("Basket_res");
 const quantity_res = document.getElementById("quantity_res");
